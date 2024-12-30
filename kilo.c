be706b3b8b02627a323de3f716c547b29dd77c6b
neet-007 <moayedeffect@gmail.com>
2024-12-30 19:57:20 +0300

    fixed status bar color to be inverse

--- kilo.c
+++ kilo.c
@@ -980,6 +980,7 @@ void editorDrawRows(struct abuf *ab){
 }
 
 void editorDrawStatusBar(struct abuf *ab){
+    abAppend(ab, "\x1b[7m", 4);
     char status[80], rstatus[80];
     int len = snprintf(status, sizeof(status), "%.20s - %d lines %s", E.filename ? E.filename : "[No Name]", E.numrows, E.dirty ? "(modified)" : "");
     int rlen = snprintf(rstatus, sizeof(rstatus), "%s | %d/%d", E.syntax ? E.syntax->filetype : "no ft", E.cy + 1, E.numrows);
@@ -997,6 +998,7 @@ void editorDrawStatusBar(struct abuf *ab){
         }
     }
 
+    abAppend(ab, "\x1b[m", 3);
     abAppend(ab, "\r\n", 2);
 }
 
